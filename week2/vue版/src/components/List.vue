4e23934e87bd2bdda45d49ab2b18d5886d6c539c
dream1994 <646380243@qq.com>
2020-05-18 16:57:29 +0800

    mock

--- week2/vue版/src/components/List.vue
+++ week2/vue版/src/components/List.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>这里是返回结果</h1>
+    <h1>返回结果来了</h1>
     <div>
       <!-- 这里已经完成了双向绑定 -->
       <!-- 用户输入的page与num，可以通过send方法传递HTTP请求 -->
@@ -16,7 +16,7 @@
       <button type="button" @click="send()">提交</button>
     </div>
     <!-- 通过JSON.stringify格式化显示 -->
-    <pre v-html="JSON.stringify(items, null, 2)"></pre>
+    <pre v-html="JSON.stringify(this.items, null, 2)"></pre>
   </div>
 </template>
 
@@ -28,7 +28,7 @@ export default {
   data() {
     return {
       page: 1,
-      num: 10,
+      num: 3,
       // 这里是数据体
       items: []
     };
@@ -37,8 +37,15 @@ export default {
     send () {
       // 使用axios作为http客户端，发送HTTP的GET请求
       // 传递参数num和page
-      axios.get('http://your.domain.com/test')
-    .then(res => (this.items = res.data.users));
+      axios.get('http://api/yourdomain.test',{
+        params:{
+          num: this.num
+        }
+      }).then(
+        (res)=>{
+          this.items = res.data.data
+        }
+      );
     }
   }
 };
