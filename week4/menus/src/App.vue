8028313fa8c675e941d221257e000da781b63c4e
dream1994 <646380243@qq.com>
2020-05-21 23:21:35 +0800

    menus

---
+++ week4/menus/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <ul>
+      <li>
+        <router-link to="/">menus</router-link>
+      </li>
+      <li>
+        <router-link to="/order">order</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'app',
+    components: {
+      
+    },
+    mounted(){
+      // console.log(this.$store.list)
+    },
+    data () {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style>
+  *{
+    padding: 0;
+    margin: 0;
+  }
+  a{
+    text-decoration: none;
+  }
+  
+</style>
